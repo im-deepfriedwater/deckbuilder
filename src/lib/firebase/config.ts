00194eceefc37c1ba79129f8960e8206d0b8bcf6
im-deepfriedwater <justintorres56@gmail.com>
2024-12-07 15:36:43 -0800

    config setup

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,10 +1,25 @@
+require('dotenv').config()
 
-// When deployed, there are quotes that need to be stripped
-Object.keys(config).forEach((key: string) => {
-  const configValue = config[key] + "";
-  if (configValue.charAt(0) === '"') {
-    config[key] = configValue.substring(1, configValue.length - 1);
-  }
-});
+import { config as dotenvConfig } from 'dotenv';
 
+dotenvConfig();
+
+const config = {
+  apiKey: process.env['apiKey'],
+  authDomain: process.env['authDomain'],
+  projectId: process.env['projectId'],
+  storageBucket: process.env['storageBucket'],
+  messagingSenderId: process.env['messagingSenderId'],
+  appId: process.env['appId'],
+  measurementId: process.env['measurementId'],
+};
+
+// // When deployed, there are quotes that need to be stripped
+// Object.keys(config).forEach((key: string) => {
+//   const configValue = config[key] + "";
+//   if (configValue.charAt(0) === '"') {
+//     config[key] = configValue.substring(1, configValue.length - 1);
+//   }
+// });
 export const firebaseConfig = config;
+
