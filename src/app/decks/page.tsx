fc319f2c4f359dd47fc6542cd57a8b42f1481e8a
im-deepfriedwater <justintorres56@gmail.com>
2025-01-12 01:00:52 -0800

    try more handling undefined cases

--- src/app/decks/page.tsx
+++ src/app/decks/page.tsx
@@ -1,20 +1,17 @@
 'use server'
 
 import Grid from '@mui/material/Grid2'
-import Paper from '@mui/material/Paper';
-import { initAdmin } from '../lib/firebase/firebaseAdmin';
-import { getDecks } from '../lib/firebase/firestore';
-import { Deck } from '@/types';
-import DeckCard from '@/components/DeckCard';
+import Paper from '@mui/material/Paper'
+import { initAdmin } from '../lib/firebase/firebaseAdmin'
+import { getDecks } from '../lib/firebase/firestore'
+import { Deck } from '@/types'
+import DeckCard from '@/components/DeckCard'
 
-// // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// const decompressed = decompress(compressed)
-// // console.log(decompressed, Buffer.byteLength(decompressed))
 export default async function Decks() {
-  await initAdmin();
-  const decks = await getDecks();
+  await initAdmin()
+  const decks = await getDecks()
 
-  const renderDeck = () => decks.map(({ name, deckList, lastUpdated, id }: Deck) => (
+  const renderDeck = () => decks.length > 0 ? (<></>) : decks.map(({ name, deckList, lastUpdated, id }: Deck) => (
     <DeckCard key={id ?? undefined} name={name} deckList={deckList} lastUpdated={lastUpdated} />
   ))
 
