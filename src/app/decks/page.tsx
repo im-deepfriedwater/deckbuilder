da5d96f0f0930214d505826e7d9efb407ddbfbf8
im-deepfriedwater <justintorres56@gmail.com>
2025-01-12 01:05:06 -0800

    test deployment remove firebase server init

--- src/app/decks/page.tsx
+++ src/app/decks/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use server'
 
 import Grid from '@mui/material/Grid2'
@@ -8,17 +9,17 @@ import { Deck } from '@/types'
 import DeckCard from '@/components/DeckCard'
 
 export default async function Decks() {
-  await initAdmin()
-  const decks = await getDecks()
+  // await initAdmin()
+  // const decks = await getDecks()
 
-  const renderDeck = () => decks.length > 0 ? (<></>) : decks.map(({ name, deckList, lastUpdated, id }: Deck) => (
-    <DeckCard key={id ?? undefined} name={name} deckList={deckList} lastUpdated={lastUpdated} />
-  ))
+  // const renderDeck = () => decks.length > 0 ? (<></>) : decks.map(({ name, deckList, lastUpdated, id }: Deck) => (
+  //   <DeckCard key={id ?? undefined} name={name} deckList={deckList} lastUpdated={lastUpdated} />
+  // ))
 
   return (
     <Paper className='m-12 h-[80vh]'>
       <Grid container spacing={8}>
-        { renderDeck() }
+        {  }
       </Grid>
     </Paper>
   )
